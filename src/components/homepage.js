5cf4cec14aed6bf47de359bab963c48122cd3c64
Arunima Barik <arunimabarik75@gmail.com>
2023-04-20 15:30:12 +0530

    merge 2

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -24,11 +24,7 @@ export default function homepage() {
                   </tr>
                   <tr><br /><br /></tr>
                   <tr><td colSpan='3'>
-<<<<<<< HEAD
-                  <NavLink to='/information'><Button variant='outline-danger' className='button-text' size='lg'>&nbsp;&nbsp;Know more about the algorithm&nbsp;&nbsp;</Button></NavLink></td></tr>
-=======
                   <NavLink to='/information'><Button variant='outline-danger' className='button-text' id='intro_page_know' size='lg'><span>&nbsp;&nbsp;Know More about the algorithm&nbsp;&nbsp;</span></Button></NavLink></td></tr>
->>>>>>> 61b94d6ed93344fe722fe61ab951d5989cab5585
                   <tr><br /></tr>
                   <tr><td colSpan='3'>
                   <NavLink to='/example'><Button variant='outline-success' className='button-text' id='intro_page_try' size='lg'><span>&nbsp;&nbsp;Try an example&nbsp;&nbsp;</span></Button></NavLink></td></tr>
